5f4788acd0a5a548e9b13068fb8941cc09db5786
LakshmiSai <lakshmi.peddi@zensar.com>
2020-08-06 10:36:23 +0530

    Applying Commit

---
+++ Assignment5-8-2020/StrCollection/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrCollection
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCollection str = new StringCollection();
+            str.Add("Sai");
+            str.Add("Lakshmi");
+            str.Add("Vinni");
+            str.Add("Hari");
+            str.Add("Teju");
+            Console.WriteLine("StringCollection elements");
+            foreach (string res in str)
+            {
+                Console.WriteLine(res);
+            }
+            String[] arr = new String[] { "one", "two", "three", "four", "five" };
+            str.AddRange(arr);
+            Console.WriteLine("Adding range of elements to string collection.....");
+            foreach (string res in str)
+            {
+                Console.WriteLine(res);
+            }
+            Console.WriteLine("inserting elements at a specified location.....");
+            str.Insert(1, "Sampad");
+            foreach (string res in str)
+            {
+                Console.WriteLine(res);
+            }
+            Console.WriteLine("checking wheteher the element is present or nor...");
+            var k = str.Contains("Siri");
+            Console.WriteLine(k);
+            Console.WriteLine("finding the index of a particular element...");
+            var j = str.IndexOf("Geetha");
+            Console.WriteLine(j);
+            Console.WriteLine("finding equality...");
+            var l = str.Equals("Sarika");
+            Console.WriteLine(l);
+            Console.Read();
+        }
+    }
+}

ab85302da299fa3c016816e95f67ffb4d36461b9
LakshmiSai <lakshmi.peddi@zensar.com>
2020-08-09 23:28:21 +0530

    Commit Changes

---
+++ Assignment6-8-2020/ConParseAndInt32/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConParseAndInt32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string convertToInt = "12";
+            string nullString = null;
+            string maxValue = "59781346792584139761";
+            string formatException = "45.98";
+
+            Console.WriteLine("int.Parse");
+            Console.WriteLine();
+            Console.WriteLine(" int.Parse(convertToInt) : {0}", int.Parse(convertToInt));
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine("int.Parse(nullString) : {0}", int.Parse(nullString));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine("int.Parse(maxValue) : {0}", int.Parse(maxValue));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine("int.Parse(formatException) : {0}", int.Parse(formatException));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+
+            Console.WriteLine("ConvertToInt");
+            Console.WriteLine();
+            Console.WriteLine("Convert.ToInt32(convertToInt) : {0}", Convert.ToInt32(convertToInt));
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine(" Convert.ToInt32(nullString) : {0}", Convert.ToInt32(nullString));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine(" Convert.ToInt32(maxValue) : {0}", Convert.ToInt32(maxValue));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine(" Convert.ToInt32(formatException) : {0}", Convert.ToInt32(formatException));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment7-8-2020/FilesAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FilesAssignment
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string path = @"E:\DotNet\ConsoleFindouts\Samples\MyTest.txt";
+
+
+            if (!File.Exists(path))
+            {
+
+                string createText = "This is Sai!";
+                File.WriteAllText(path,createText);
+            }
+
+
+            string appendText = "Hello Sai";
+            File.AppendAllText(path,appendText);
+
+            File.Copy(@"E:\DotNet\ConsoleFindouts\Samples\MyTest.txt", @"E:\DotNet\ConsoleFindouts\Samples\MyTest2.txt");
+            string readText = File.ReadAllText(path);
+
+            File.Delete(@"E:\DotNet\ConsoleFindouts\Samples\MyTest2.txt");
+            Console.WriteLine(readText);
+        }
+    }
+}
